490e67577ae73f12a164a555a92dea411113c15e
vikulchik <kvvendetta@gmail.com>
2016-02-09 20:35:14 +0200

    Add qtip.

--- dev/js/app.js
+++ dev/js/app.js
@@ -11421,22 +11421,27 @@ addProject.init();
 ;(function(){
 
   $("#pop-up_add-new-project").on('submit',  validate);
-  $("input").keydown(removeTooltips);
+  $("input, textarea").on('keydown', removeTooltips);
 
   function validate(){
     var inputs = $(this).find('input, textarea');
     inputs.each(function(){
       var $this = $(this);
       if ($this.val().length < 1){
-        $('.qtip').removeClass("hide");
+        $('.tooltip').removeClass("hide");
         $this.addClass('error');
+        $(".pop-up_label-download").addClass('error');
       }
     });
 
   }
 
   function removeTooltips(){
-    $(this).next();
+   $(this).next().addClass('hide');
+   $(this).removeClass('error');
+   if ($('input').attr('type') === 'file'){
+    $(this).removeClass('error');
+   }
   }
 
 }());
