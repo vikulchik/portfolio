97c24b46f29d0aabd654b105a78f9b3ef7539c61
vikulchik <kvvendetta@gmail.com>
2016-02-09 14:19:14 +0200

    Add qtip.

--- dev/js/app.js
+++ dev/js/app.js
@@ -11421,17 +11421,22 @@ addProject.init();
 ;(function(){
 
   $("#pop-up_add-new-project").on('submit',  validate);
+  $("input").keydown(removeTooltips);
 
   function validate(){
     var inputs = $(this).find('input, textarea');
+    inputs.each(function(){
+      var $this = $(this);
+      if ($this.val().length < 1){
+        $('.qtip').removeClass("hide");
+        $this.addClass('error');
+      }
+    });
+
   }
 
-
-  inputs.each(function(){
-    var $this = $(this);
-      if ($this.val().length < 1){
-        $this.addClass("qtip");
-      }
-  });
+  function removeTooltips(){
+    $("input").removeClass('hide');
+  }
 
 }());
